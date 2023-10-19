416b41f57b7889858669c013a50b512efcbdbff1
saber <106769715+SabMehdi@users.noreply.github.com>
2023-10-19 03:46:49 +0200

    cors, tweaking api

--- server/settings.py
+++ server/settings.py
@@ -27,7 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = False
 
+# Whitelist the origins that are allowed to access your Django API:
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Add your React frontend origin here
+    # Add other origins if needed
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken'
 ]
 
@@ -49,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
--- server/views.py
+++ server/views.py
@@ -12,12 +12,12 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def login(request):
-    user=get_object_or_404(User,username=request.data['username'])
+    user=get_object_or_404(User,email=request.data['email'])
     if not user.check_password(request.data['password']):
-        return Response({"detail":"wrong credentials"},status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail":"wrong password"},status=status.HTTP_400_BAD_REQUEST)
     token,created=Token.objects.get_or_create(user=user)
     serializer=UserSerializer(instance=user)
-    return Response({"token":token.key,"user":serializer.data})
+    return Response({"token":token.key,"email":serializer.data['email'],"username":serializer.data['username']})
 
 @api_view(['POST'])
 def signup(request):
@@ -28,7 +28,7 @@ def signup(request):
         user.set_password(request.data['password']) #sauvegarde du pw cripté
         user.save()
         token=Token.objects.create(user=user)
-        return Response({"token":token.key,"user": serializer.data})
+        return Response({"token":token.key,"email":serializer.data['email'],"username":serializer.data['username']})
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
